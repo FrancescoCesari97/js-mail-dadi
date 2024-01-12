b22eb3f4f152b34a0540f169856bd95047c6fb6f
FrancescoCesari97 <freedus97@gmail.com>
2024-01-12 15:59:50 +0100

    aggiunto esercizio email

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
+const result = document.getElementById("result");
+result.innerHTML = "";
+
 // creare una lista di email con un Array
 
 const emails = ["Wind@gmail.com", "Fire@gmail.com", "Water@gmail.com"];
 console.log(emails);
+
+// chiedere con un promt l'email all'utente
+
+const emailUser = prompt("inserisci la tua email");
+console.log(emailUser);
+
+// verificare se l'email inserita
+if (
+  emailUser == emails[0] ||
+  emailUser == emails[1] ||
+  emailUser == emails[2]
+) {
+  result.innerHTML = ` <h1> Accesso approvato </h1>`;
+} else {
+  if (
+    emailUser != emails[0] ||
+    emailUser != emails[1] ||
+    emailUser != emails[2]
+  ) {
+    result.innerHTML = ` <h1> Accesso Negato</h1>`;
+    emailUser = prompt("email sbagliata riprovare ad inserire");
+  }
+  //
+}
